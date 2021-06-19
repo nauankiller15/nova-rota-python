9690305e715395006219f869a0b995c40fe08fd2
nauankiller15 <nauan.alcantara1@gmail.com>
2021-06-19 13:42:30 -0300

    alteracoes para trabalhar em grupo

---
+++ nova-rota-backend/todo/variaveis_de_ambiente.py
@@ -0,0 +1,16 @@
+SECRET_KEY = 'django-insecure-&p(sim^*%3ja)0x*uchqe36&_b!&%dryu%!!0jez@pi72v_h*m'
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'nrs-sistema',
+        'USER': 'postgres',
+        'PASSWORD': 'bananaverde1234',
+        'HOST': 'localhost',
+        'PORT': '5432'
+
+    }
+}
