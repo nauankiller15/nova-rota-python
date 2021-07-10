690f85b342d8d45dc8ef652bd88f3a9f336c597f
nauankiller15 <nauan.alcantara1@gmail.com>
2021-07-10 00:16:01 -0300

    ajuste no css da api

--- nova-rota-backend/todo/settings.py
+++ nova-rota-backend/todo/settings.py
@@ -144,7 +144,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_ROOT = 'imagens'
 
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
