94d3b1ff09f8aef8c3bc753c32b5da85c1aba13c
nauankiller15 <nauan.alcantara1@gmail.com>
2021-07-10 16:59:40 -0300

    ajuste na senha do db

--- nova-rota-backend/todo/settings.py
+++ nova-rota-backend/todo/settings.py
@@ -95,8 +95,8 @@ DEBUG = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'nrssistema',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'nrsbanco',
         'USER': 'djangoadm',
         'PASSWORD': 'sistemanrs12',
         'HOST': 'localhost',
@@ -104,11 +104,6 @@ DATABASES = {
 
     }
 }
-
-#'local': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3e',
-#    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
