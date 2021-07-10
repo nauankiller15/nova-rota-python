bac281ef9f4b45f2dd5617c1b77a6b546dc38c54
nauankiller15 <nauan.alcantara1@gmail.com>
2021-07-09 23:52:46 -0300

    adicionando ip no allowed hosts

--- nova-rota-backend/todo/settings.py
+++ nova-rota-backend/todo/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
    }
 }
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['35.247.213.199', 'localhost', '127.0.0.1']
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
