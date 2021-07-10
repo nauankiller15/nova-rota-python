e8d171decc6d65dc29003361a33e521ceb57c246
nauankiller15 <nauan.alcantara1@gmail.com>
2021-07-10 16:46:31 -0300

    ajuste na senha do db

--- nova-rota-backend/todo/settings.py
+++ nova-rota-backend/todo/settings.py
@@ -102,12 +102,13 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': ''
 
-    },
-    'local': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3e',
     }
 }
+
+#'local': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3e',
+#    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
