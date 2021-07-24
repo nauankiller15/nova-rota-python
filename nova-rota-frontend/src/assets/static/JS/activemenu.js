f6b4fe95bef2bea0c5ba81df6b6502d7c953cbfc
nauankiller15 <nauan.alcantara1@gmail.com>
2021-07-24 15:23:20 -0300

    ajustes

--- nova-rota-frontend/src/assets/static/JS/activemenu.js
+++ nova-rota-frontend/src/assets/static/JS/activemenu.js
@@ -1,10 +1,17 @@
+
+window.onload = function() {
+  if(!window.location.hash) {
+      window.location = window.location + '#loaded';
+      window.location.reload();
+  }
+}
 //
 //
 //
 // TELA DE CARREGAMENTO
 $(window).on("load", function () {
   
-  $(".loader-wrapper").fadeOut("slow");
+    $(".loader-wrapper").fadeOut("slow");
   $('form').each(function () {
     this.reset();
   });
