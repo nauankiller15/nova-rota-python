6822dbfceedd40e72e93182f55050d5fceab58d9
Daniel Chaves de Lima <danielchaves001@gmail.com>
2021-07-27 11:44:09 -0300

    finalizada a verificação de cpf e cnpj

--- nova-rota-frontend/src/app/shared/validador-cnpj.ts
+++ nova-rota-frontend/src/app/shared/validador-cnpj.ts
@@ -1,21 +1,29 @@
 export function validarCNPJ(cnpj:string): boolean {
     if (cnpj.length == 18) {
-        const numeroCNPJ = cnpj.replace(/\./g, '').replace('-', '');
+        const numeroCNPJ = cnpj.replace(/\./g, '').replace('/', '').replace('-', '');
+        const multiplicadores = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
         
-        let soma = 0;
-        for (let i = 11; i >= 2; i--) {
-            soma += parseInt(numeroCNPJ[11 - i]) * i;
+        const digitos = new Set(numeroCNPJ);
+        if (digitos.size <= 1) {
+            return false
         }
 
-        const digito1 = ((soma * 11) % 14) % 11;
-        if (digito1 == parseInt(numeroCNPJ[10])) {
-            soma = 0
-            for (let i = 14; i >= 2; i--) {
-                soma += parseInt(numeroCNPJ[14- i]) * i;
-            }
+        let soma = 0;
+        for (let i = 0; i < 12; i++) {
+            soma += parseInt(numeroCNPJ[i]) * multiplicadores[i+1];
+        }
+
+        const digito1 = (soma * 10 % 11) % 10;
         
-            const digito2 = ((soma * 11) % 14) % 11;
-            if (digito2 == parseInt(numeroCNPJ[11])) {
+        if (digito1 == parseInt(numeroCNPJ[12])) {
+            soma = 0;
+            for (let i = 0; i < 13; i++) {
+                soma += parseInt(numeroCNPJ[i]) * multiplicadores[i];
+            }
+
+            const digito2 = (soma * 10 % 11) % 10;
+            
+            if (digito2 == parseInt(numeroCNPJ[13])) {
                 return true
             }
         }
--- nova-rota-frontend/src/app/shared/validador-cpf.ts
+++ nova-rota-frontend/src/app/shared/validador-cpf.ts
@@ -1,20 +1,12 @@
 export function validarCPF(cpf: string): boolean {
   if (cpf.length == 14) {
     const numeroCPF = cpf.replace(/\./g, '').replace('-', '');
-    if (
-      cpf == '00000000000' ||
-      cpf == '11111111111' ||
-      cpf == '22222222222' ||
-      cpf == '33333333333' ||
-      cpf == '44444444444' ||
-      cpf == '55555555555' ||
-      cpf == '66666666666' ||
-      cpf == '77777777777' ||
-      cpf == '88888888888' ||
-      cpf == '99999999999' ||
-      console.log('Certo')
-    )
-      return false;
+    
+    const digitos = new Set(numeroCPF);
+    if (digitos.size <= 1) {
+        return false
+    }
+    
     let soma = 0;
     for (let i = 10; i >= 2; i--) {
       soma += parseInt(numeroCPF[10 - i]) * i;
