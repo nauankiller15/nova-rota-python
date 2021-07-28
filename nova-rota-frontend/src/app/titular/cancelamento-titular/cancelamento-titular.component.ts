6356736c62bc3b8252d9a479b0b2bd8d089b404a
nauankiller15 <nauan.alcantara1@gmail.com>
2021-07-28 19:14:42 -0300

    retirado os loadings das telas de alterações e consultas, prioridade acrescentado em alterações e cancelamentos

--- nova-rota-frontend/src/app/titular/cancelamento-titular/cancelamento-titular.component.ts
+++ nova-rota-frontend/src/app/titular/cancelamento-titular/cancelamento-titular.component.ts
@@ -169,4 +169,18 @@ export class CancelamentoTitularComponent implements OnInit {
       }
     );
   }
+
+  tipoPrioridade(data) {
+    const hoje = new Date();
+    let dataPrioridade = new Date(data);
+    dataPrioridade.setMonth(dataPrioridade.getMonth() + 1);
+    let prioridade = 'Prioridade';
+    console.log(dataPrioridade, hoje, dataPrioridade > hoje);
+    if (dataPrioridade < hoje) {
+      prioridade = "Sem Prioridade";
+    }
+
+    return prioridade
+  }
+  
 }
