7fa4938d06c88113d3a7bc74ae34ca04cea5fdfb
Daniel Chaves de Lima <danielchaves001@gmail.com>
2021-09-11 12:33:01 -0300

    obrigatoriedade de anexos do titular

--- nova-rota-frontend/src/app/titular/novo-titular/novo-titular.component.ts
+++ nova-rota-frontend/src/app/titular/novo-titular/novo-titular.component.ts
@@ -97,14 +97,6 @@ export class NovoTitularComponent implements OnInit {
     $('#abrirAnexo4').click(function () {
       $('#vinc-anexo-empregaticio').fadeIn('100');
     });
-
-    // CONFIRMAÇÃO DECLARAÇÃO SAÚDE
-
-    $('#declaracaoSaudeTitular').on('change', function () {
-      'Sim' === $(this).val()
-        ? $('#descDeclaracaoSaudeTitular').fadeIn('100')
-        : $('#descDeclaracaoSaudeTitular').fadeOut('100');
-    });
     //
 
     $('#fecharTelaTitular').click(function () {
